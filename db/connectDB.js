1e2be35706e7ae71433d401bc1264baf3ce874f0
shinbe69 <rantarou@live.com>
2023-10-21 18:17:52 +0700

    some fixes for deploy on render

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config({ path: '../.env' })
 
 let isConnect = new Promise((resolve, reject) => {
-    mongoose.connect(process.env.DB_URIa)
+    mongoose.connect('mongodb+srv://doanquocsang:Imabelieber699@cluster0.xfjeogm.mongodb.net/?retryWrites=true&w=majority')
     .then(() => resolve(true))
     .catch(() => reject(false))
 })
